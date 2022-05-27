65e37d61c46c6d8bef426894aceac51fd83a2b1e
MusaTamzid05 <tamzid101@gmail.com>
2022-05-27 15:00:26 +0600

    create vector3.

---
+++ include/vector3.h
@@ -0,0 +1,62 @@
+#ifndef VECTOR3_H
+#define VECTOR3_H
+
+class Vector3 {
+    public:
+
+        Vector3():
+            x(0),
+            y(0),
+            z(0) {}
+
+
+        Vector3(const float x, const float y, const float z):
+            x(x),
+            y(y),
+            z(z) {}
+
+        void invert();
+        void normalize();
+
+        void operator+=(const Vector3& v);
+        Vector3 operator+(const Vector3& v);
+
+        void operator-=(const Vector3& v);
+        Vector3 operator-(const Vector3& v);
+
+        void operator*=(const float value);
+        Vector3 operator*(const float value) const;
+        float operator*(const Vector3& vector) const;
+
+
+
+        void operator%=(const Vector3& vector);
+        Vector3 operator%(const Vector3& vector) const;
+
+        void addScaledVector(const Vector3& vector, float scale);
+        Vector3 componentProduct(const Vector3& vector) const;
+        void componentProductUpdate(const Vector3& vector);
+
+        void clear();
+        void clearAccumulator();
+
+        float scalarProduct(const Vector3& vector) const;
+
+
+        float magnitude() const;
+        float squareMagnitude() const;
+
+
+        Vector3 vectorProduct(const Vector3& vector) const;
+
+        float x;
+        float y;
+        float z;
+
+    private:
+        float pad;
+
+
+
+};
+#endif
---
+++ src/vector3.cpp
@@ -0,0 +1,122 @@
+#include "vector3.h"
+#include <cmath>
+
+void Vector3::invert() {
+    x *= -1;
+    y *= -1;
+    z *= -1;
+}
+
+
+float Vector3::magnitude() const {
+    return sqrt(x * x + y * y + z * z);
+}
+
+
+float Vector3::squareMagnitude() const {
+    return x * x + y * y + z * z;
+}
+
+
+void Vector3::normalize() {
+    float len = magnitude();
+
+    if(len > 0) 
+        (*this)*= ((float)1) / len;
+}
+
+void Vector3::operator*=(const float value) {
+    x *= value;
+    y *= value;
+    z *= value;
+}
+
+
+Vector3 Vector3::operator*(const float value) const {
+    return Vector3(x * value, y * value, z * value);
+}
+
+
+
+void Vector3::operator+=(const Vector3& v) {
+    x += v.x;
+    y += v.y;
+    z += v.z;
+}
+
+Vector3 Vector3::operator+(const Vector3& v) {
+    return Vector3(x + v.x, y + v.y, z + v.z);
+}
+
+
+void Vector3::operator-=(const Vector3& v) {
+    x -= v.x;
+    y -= v.y;
+    z -= v.z;
+}
+
+Vector3 Vector3::operator-(const Vector3& v) {
+    return Vector3(x - v.x, y - v.y, z - v.z);
+}
+
+
+
+void Vector3::addScaledVector(const Vector3& vector, float scale) {
+    x += vector.x * scale;
+    y += vector.y * scale;
+    z += vector.z * scale;
+}
+
+
+Vector3 Vector3::componentProduct(const Vector3& vector) const {
+    return Vector3(x * vector.x, y * vector.y, z * vector.z);
+}
+
+
+void Vector3::componentProductUpdate(const Vector3& vector) {
+    x *= vector.x;
+    y *= vector.y;
+    z *= vector.z;
+}
+
+void Vector3::clear() {
+    x = 0;
+    y = 0;
+    z = 0;
+}
+
+
+float Vector3::scalarProduct(const Vector3& vector) const {
+    return x * vector.x + y * vector.y + z * vector.z;
+
+}
+
+
+float Vector3::operator*(const Vector3& vector) const {
+    return x * vector.x + y * vector.y + z * vector.z;
+}
+
+
+
+Vector3 Vector3::vectorProduct(const Vector3& vector) const {
+    return Vector3(
+                y * vector.z - z * vector.y,
+                z * vector.x - x * vector.z,
+                x * vector.y - y * vector.x
+            );
+}
+
+
+void Vector3::operator%=(const Vector3& vector) {
+    *this = vectorProduct(vector); 
+}
+
+
+
+Vector3 Vector3::operator%(const Vector3& vector) const {
+    return Vector3(
+                y * vector.z - z * vector.y,
+                z * vector.x - x * vector.z,
+                x * vector.y - y * vector.x
+            );
+}
